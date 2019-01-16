762e9ff4c070e019fd97a3c0dee7a644926105c7
huzzzz <huzefachasmai@gmail.com>
2019-01-17 01:45:44 +0530

    Changed the covariance step bug

--- A1/assignmentShapeAnalysis/code/ellipses2D/myMainScript1.m
+++ A1/assignmentShapeAnalysis/code/ellipses2D/myMainScript1.m
@@ -54,7 +54,8 @@ plot_data_points(point_set_data_norm, num_point_sets, has_mean, mean_new, 'Mean_
 	
 % Covariance Analysis
 
-point_set_data_norm_flat = reshape(point_set_data_norm, [dim*num_points, num_point_sets]);
+point_set_data_norm_flat = reshape(point_set_data_norm - repmat(mean_new, [1,1,num_point_sets]), [dim*num_points, num_point_sets]);
+% point_set_data_norm_flat = point_set_data_norm_flat - );
 cov_mat = double(point_set_data_norm_flat * point_set_data_norm_flat') ./ double(num_point_sets);
 % size(cov_mat)
 
@@ -83,12 +84,12 @@ title("Modes of variation 1 with the mean");
 
 hold on;
 for i = 1:num_point_sets
-	scatter(point_set_data(1,:, i), point_set_data(2, :, i), 10, [1,1,0], 'filled');
+	scatter(point_set_data_norm(1,:, i), point_set_data_norm(2, :, i), 10, [1,1,0], 'filled');
 end	
 
-scatter(mean_new(1,:), mean_new(2,:), 50, [1, 0, 0], 'filled', 'd');
-scatter(mode_of_var1_inc(1,:), mode_of_var1_inc(2,:), 50, [0, 1, 0], 'filled', 'd');
-scatter(mode_of_var1_dec(1,:), mode_of_var1_dec(2,:), 50, [0, 0, 1], 'filled', 'd');
+scatter(mean_new(1,:), mean_new(2,:), 10, [1, 0, 0], 'filled', 'd');
+scatter(mode_of_var1_inc(1,:), mode_of_var1_inc(2,:), 10, [0, 1, 0], 'filled', 'd');
+scatter(mode_of_var1_dec(1,:), mode_of_var1_dec(2,:), 10, [0, 0, 1], 'filled', 'd');
 
 hold off;
 saveas(f, "Modes_of_variation1.png");
@@ -110,17 +111,16 @@ hold on;
 title("Modes of variation 2 with the mean");
 
 for i = 1:num_point_sets
-	scatter(point_set_data(1,:, i), point_set_data(2, :, i), 10, [1,1,0], 'filled');
+	scatter(point_set_data_norm(1,:, i), point_set_data_norm(2, :, i), 10, [1,1,0], 'filled');
 end	
 
-scatter(mean_new(1,:), mean_new(2,:), 50, [1, 0, 0], 'filled', 'd');
-scatter(mode_of_var2_inc(1,:), mode_of_var2_inc(2,:), 50, [0, 1, 0], 'filled', 'd');
-scatter(mode_of_var2_dec(1,:), mode_of_var2_dec(2,:), 50, [0, 0, 1], 'filled', 'd');
+scatter(mean_new(1,:), mean_new(2,:), 10, [1, 0, 0], 'filled', 'd');
+scatter(mode_of_var2_inc(1,:), mode_of_var2_inc(2,:), 10, [0, 1, 0], 'filled', 'd');
+scatter(mode_of_var2_dec(1,:), mode_of_var2_dec(2,:), 10, [0, 0, 1], 'filled', 'd');
 
 hold off;
 
 saveas(f, "Modes_of_variation2.png");
 close(f);
-
 
 toc;
